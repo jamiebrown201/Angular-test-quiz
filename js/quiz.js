797ec60abfef825faa6f78e045d3b6fefc048bb4
Jamie Brown <jamiebrown201@hotmail.com>
2016-03-09 12:31:02 +0000

    controller variables added

--- js/quiz.js
+++ js/quiz.js
@@ -4,7 +4,14 @@
 
 	app.controller('QuizController', ['$scope', '$http', '$sce',function($scope, $http, $sce){
 
-	}]);
+		$scope.score = 0;
+		$scope.activeQuestion = -1;
+		$scope.activeQuestionAnswered = 0;
+		$scopre.percentage = 0;
 
+
+
+
+	}]);
 
 })();
