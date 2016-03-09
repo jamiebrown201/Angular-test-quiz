2360bb8be1d2c364e6f59e64c189552331fcf472
Jamie Brown <jamiebrown201@hotmail.com>
2016-03-09 13:43:56 +0000

    angular init

--- js/quiz.js
+++ js/quiz.js
@@ -5,10 +5,14 @@
 	app.controller('QuizController', ['$scope', '$http', '$sce',function($scope, $http, $sce){
 
 		$scope.score = 0;
-		$scope.activeQuestion = -1;
-		$scope.activeQuestionAnswered = 0;
-		$scopre.percentage = 0;
+    $scope.activeQuestion = -1;
+    $scope.activeQuestionAnswered = 0;
+    $scope.percentage = 0;
 
+    $http.get('quiz_data.json').then(function(quizData){
+      $scope.myQuestions = quizData.data;
+      $scope.totalQuestions = $scope.myQuestions.length;
+    });
 
 
 
